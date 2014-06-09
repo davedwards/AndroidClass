e6c1a05ad70138b890a3e9112bde1403e01bd9ad
Dave Edwards <davedwardz@gmail.com>
2014-06-08 21:54:17 -0700

    added delete confirmation dialog

---
+++ ToDoList/src/com/android/mytodolist/Form.java
@@ -0,0 +1,110 @@
+package com.android.mytodolist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.android.todolist.R;
+
+public class Form extends Activity {
+	private EditText title;
+	private EditText description;
+//	private TextView dateText;
+//	private TextView timeText;
+	private Button enter;
+	private DBHelper helper;
+	private DatePicker date;
+	private TimePicker time;
+	int mode = 0;
+	long _id = -1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.schedule);
+		
+		enter = (Button) findViewById(R.id.save);
+		title = (EditText) findViewById(R.id.title);
+		description = (EditText) findViewById(R.id.description);
+		date = (DatePicker) findViewById(R.id.calendar);
+		time = (TimePicker) findViewById(R.id.timePick);
+		time.setIs24HourView(false);
+
+		Bundle extras = getIntent().getExtras();
+		if (extras != null) {
+
+			mode = extras.getInt(ScheduleEntry.MODE);
+
+			if (mode == ScheduleEntry.EDIT) {
+				String titleString = extras
+						.getString(ScheduleEntry.COLUMN_NAME_TITLE);
+				String descriptionString = extras
+						.getString(ScheduleEntry.COLUMN_NAME_DESCRIPTION);
+				
+				_id = extras.getLong(ScheduleEntry.COLUMN_NAME_ENTRY_ID);
+
+				title.setText(titleString);
+				description.setText(descriptionString);
+			}
+
+		}
+
+		helper = new DBHelper(this);
+
+		enter.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// get information from form
+				String ttl = title.getEditableText().toString();
+				String des = description.getEditableText().toString();
+				
+				int getYear = date.getYear();
+				int getMonth = date.getMonth();
+				int getDay = date.getDayOfMonth();
+				
+				int getHour = time.getCurrentHour();
+				int getMinutes = time.getCurrentMinute();
+				int dbHour = getHour % 12;
+				String ampm;
+				
+				if(dbHour == 0) {
+					dbHour = 12;
+				}
+				
+				if(getHour > 11){
+					ampm = "PM";
+				} else {
+					ampm = "AM";
+				}
+				
+				String dateValue = getYear + "-" + getMonth + "-" + getDay;
+				String timeValue = dbHour + ":" + getMinutes + " " + ampm;
+				
+				if(mode == ScheduleEntry.EDIT){
+					helper.updateSchedule(_id, ttl, des, dateValue, timeValue);
+				}else{
+					helper.insertSchedule(ttl, des, dateValue, timeValue);
+
+				}	
+				
+				Intent intent = new Intent(Form.this, ScheduleListActivity.class);
+				startActivity(intent);
+			}
+		});
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		helper.close();
+	}
+}
